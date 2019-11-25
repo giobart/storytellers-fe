f865e3de751387b2b39c66df56545ec91b3cfe71
Laura <l.bussi1@studenti.unipi.it>
2019-11-25 13:38:54 +0100

    :new: added signup template

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import os
 import views
 
 from flask import Flask
+#from flask_bootstrap import Bootstrap
 
 
 def create_app():
     app = Flask(__name__)
+    #bootstrap = Bootstrap(app)
 
     # Registration of the error handlers
     from views import errors
--- static/scripts/requests.js
+++ static/scripts/requests.js
@@ -1,5 +1,7 @@
 API_GATEWAY = "http://localhost:8081"
 LOGIN_API = "/login"
+SIGNUP_API = "/signup"
+FRONT_END = "http://localhost:5000"
 
 function login() {
     user = {
@@ -32,6 +34,48 @@ function login_response_callback(data, status, xhr) {
     if (status == 200) {
         sessionStorage.setItem("username", sessionStorage.getItem("tmp_username"))
         sessionStorage.setItem("password", sessionStorage.getItem("tmp_password"))
+        //window.location.href = '/';
+    } else {
+        $('#credential-error').show()
+    }
+
+}
+
+function signup() {
+    new_user = {
+        email: $("#email").val(),
+        username: $("#username").val(),
+        password: $("#password").val()
+    }
+
+    $('#signup-error').show()
+
+    // Login automatically on Signup?
+    //sessionStorage.setItem("tmp_username", user.username)
+    //essionStorage.setItem("tmp_password", user.password)
+
+    $.ajax({
+        type: "POST",
+        url: API_GATEWAY + SIGNUP_API,
+        // The key needs to match your method's input parameter (case-sensitive).
+        data: JSON.stringify(user),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: signup_response_callback,
+        failure: function (errMsg) {
+            $('#signup-error').show();
+        }
+    });
+
+}
+
+function signup_response_callback(data, status, xhr) {
+
+    if (status == 200) {
+        //Redirect to the Login page
+        window.location.replace(FRONT_END + LOGIN_API)
+        //sessionStorage.setItem("username", sessionStorage.getItem("tmp_username"))
+        //sessionStorage.setItem("password", sessionStorage.getItem("tmp_password"))
         //window.location.href = '/';
     } else {
         $('#credential-error').show()
