7f68e1671aee2777797a0945ab561b612f3311b0
Giovanni <giovanni.bartolomeo.rg@gmail.com>
2019-11-26 01:52:20 +0100

    Merge branch 'develop' of https://github.com/giobart/storytellers-fe into develop
    # Conflicts:
    #	static/scripts/requests.js
    #	templates/signup.html
    
    implemented logout

--- static/scripts/requests.js
+++ static/scripts/requests.js
@@ -1,6 +1,7 @@
 API_GATEWAY = "http://localhost:8081"
 LOGIN_API = "/login"
 SIGNUP_API = "/signup"
+LOGOUT_API = "/logout"
 FRONT_END = "http://localhost:5000"
 
 function login() {
@@ -31,9 +32,10 @@ function login() {
 
 function login_response_callback(data, status, xhr) {
 
-    if (xhr.status==200) {
+    if (xhr.status == 200) {
         sessionStorage.setItem("username", sessionStorage.getItem("tmp_username"))
         sessionStorage.setItem("password", sessionStorage.getItem("tmp_password"))
+        sessionStorage.setItem("id", parseInt(data.id))
         window.location.href = '/';
     } else {
         $("#credentialError").show()
@@ -57,7 +59,7 @@ function signup() {
         dataType: "json",
         success: signup_response_callback,
         error: function (errMsg) {
-            if(errMsg.responseJSON.code=="E029U")
+            if (errMsg.responseJSON.code == "E029U")
                 $('#name-error').show()
             else
                 $('#mail-error').show()
@@ -68,16 +70,62 @@ function signup() {
 
 function signup_response_callback(data, status, xhr) {
 
-    switch(xhr.status) {
+    switch (xhr.status) {
         case 200:
             // Here 'replace' since we don't want the user to go back to the signup page.
-            window.location.href=LOGIN_API
+            window.location.href = LOGIN_API
         case 409:
-            if(xhr.response.error == '9E')
+            if (xhr.response.error == '9E')
                 $('#name-error').show()
             else
                 $('#mail-error').show()
             break
     }
 
+}
+
+function logout() {
+    $.ajax({
+        type: "POST",
+        url: API_GATEWAY + LOGOUT_API,
+        // The key needs to match ysour method's input parameter (case-sensitive).
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        xhrFields: {withCredentials: true},
+        crossDomain: true,
+        success: function(e){window.location.href = LOGIN_API},
+        error: function (errMsg) {
+            sessionStorage.setItem("username", "")
+            sessionStorage.setItem("password", "")
+            sessionStorage.setItem("id", -1)
+            window.location.href = LOGIN_API
+        }
+    });
+}
+
+
+function story_list() {
+    new_user = {
+        email: $("#email").val(),
+        username: $("#username").val(),
+        password: $("#password").val(),
+    }
+
+    $.ajax({
+        type: "POST",
+        url: API_GATEWAY + SIGNUP_API,
+        xhrFields: {withCredentials: true},
+        crossDomain: true,
+        // The key needs to match ysour method's input parameter (case-sensitive).
+        data: JSON.stringify(new_user),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: signup_response_callback,
+        error: function (errMsg) {
+            if (errMsg.responseJSON.code == "E029U")
+                $('#name-error').show()
+            else
+                $('#mail-error').show()
+        }
+    });
 }
